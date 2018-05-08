40b9d87caa70a9b6cebd8481aa6f1a252c409f51
SwiftKobe <folledoswag@gmail.com>
2018-05-08 19:38:52 -0400

    APIs and JSON Data Extract the JSON data from the openweathermap.org

--- FolledoAllUdemyAppBuild-20-Apps/ViewController.swift
+++ FolledoAllUdemyAppBuild-20-Apps/ViewController.swift
@@ -11,74 +11,47 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
-    @IBOutlet var bachImageView: UIImageView!
-    
+//viewDidLoad()
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let url = URL(string: "http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=a8d45e0155d745e7ae0c57539dcae2ea")
         
-//restoring image //EASTWOOD
-        let documentsPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
-        if documentsPath.count > 0 {
-            let documentDirectory = documentsPath[0]
-            let restorePath = documentDirectory + "/bach.jpg"
-            //now we make a UIImage from a file
-            bachImageView.image = UIImage(contentsOfFile: restorePath)
-/*
-            // THIS IS FOR SAVEPATH, but doesnt need do-try-catch
-                try UIImageJPEGRepresentation(bachImage, 1)?.write(to: URL(fileURLWithPath: savePath)) //quality is 0-1, for best quality, put in 1 //then take that image and write it to a url, and the url is going to be a url from a file with the path savePAth
- */
-            
-            
-        }
-        
-        
-        
-        let url = URL(string: "https://www.tokyoshop.es/data/productos/merchandising/MERCH4969-D.jpg")
-        let request = NSMutableURLRequest(url: url!)
-        
-        let task = URLSession.shared.dataTask(with: request as URLRequest) { //URLSession is like opening a browser window to go online and download some data (in here is image)
-            data, response, error in
+        //create a task from a URL without having to create a request
+        let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in //from the url
             
             if error != nil {
-                print (error!)
+                print(error!)
             } else {
-                if let data = data {
-                    if let bachImage = UIImage(data: data) {
-                        self.bachImageView.image = bachImage //now display the image to the UIImageView
+                if let urlContent = data { //this url content is going to be JSON
+                    
+                    do { //JSONSerialization is something that can go wrong if your data is malformed, so we need to surround it with do-try-catch
+                        let jsonResult = try JSONSerialization.jsonObject(with: urlContent, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject //JSONSerialization is an object that converts between JSON and the equivalent Foundation objects. You use the JSONSerialization class to convert JSON to Foundation objects and convert Foundation objects to JSON.
+                        print(jsonResult)
                         
-                        //Next thing to do is save the image locally in the device so that the next time the app is loaded the image is restored not from the web but from the device itself
-                        let documentsPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true) //serch for local directories on within the app itself, and we are searching for .documentDirectory. This domain mask is where we're looking for our files where we're seaching for, and we want to search relative to the user's home directory. //NSSearchPathForDirectoriesInDomains Creates a list of directory search paths. //Creates a list of path strings for the specified directories in the specified domains. The list is in the order in which you should search the directories. If expandTilde is true, tildes are expanded as described in expandingTildeInPath. //Note = The directory returned by this method may not exist. This method simply gives you the appropriate location for the requested directory. Depending on the application’s needs, it may be up to the developer to create the appropriate directory and any in between.
-                        //documentPath should search for a path, and return a single path, but it is worth checking
-                        if documentsPath.count > 0 {
-                            
-//saving the image
-                            let documentDirectory = documentsPath[0]
-                            let savePath = documentDirectory + "/bach.jpg"
-                            do {
-                            try UIImageJPEGRepresentation(bachImage, 1)?.write(to: URL(fileURLWithPath: savePath)) //quality is 0-1, for best quality, put in 1 //then take that image and write it to a url, and the url is going to be a url from a file with the path savePAth
-                            } catch {
-                                print("No UIImageJPEGRepresentation found")
-                            }
-                            
+                        print(jsonResult["name"]) //to print out the name
+                        
+                        //simplest way to extract description
+                        if let description = ((jsonResult["weather"] as? NSArray)?[0] as? NSDictionary)?["description"] as? String {//0 because weather is considered as an array with only one object, and that object is a dictionary from which we can grab the description
+                            print(description)
                         }
-                    }
+                        
+                    } catch { print("JSON Processing failed") }
                 }
             }
+            
         }
-        task.resume()//what actually runs the task
+        task.resume()
         
         
     }
+    
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
-    
-    
-    
-    
-}
 
+}
 
